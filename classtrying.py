b2db110cb0a3bcd2ac1ebedcc0943ae54e32ae68
Khusbu jaiswal <khj98047@gmail.com>
2022-10-12 12:09:40 +0545

    new

---
+++ classtrying.py
@@ -0,0 +1,74 @@
+#Ques no.1
+# print ("Helllo World python")
+
+#Ques no 45 while loop
+# i= 1
+# while i<=11:
+#     print(i)
+#     i+=1
+
+#for loop
+# for i in range(1,11):
+#     print(i)
+
+
+# Ques no 46
+# sum=0
+# for i in range(1,11):
+#     sum=sum+i
+#
+# print(sum)
+
+#Ques no 47
+#for loop
+# a=int(input("enter num"))
+# total=1
+# for i in range (a,1,-1):
+#     total = total*a
+#
+#     print("total",total)
+
+#while loop
+# a=int(input("enter num"))
+# total=1
+# while a>=1:
+#     total =a*total
+#     a=a-1
+#
+# print("total",total)
+
+# Ques 49
+# for loop
+# i=int(input("enter a num"))
+# a=0
+# b=1
+# for i in range (1,i+1):
+#     print(b)
+#     a,b=b,a+b
+
+#while loop
+i = int(input("enter a num"))
+a = 0
+b = 1
+while i >= 1:
+    print(a)
+    a, b = b, a + b
+    i-=1
+
+# Ques 50 while loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
